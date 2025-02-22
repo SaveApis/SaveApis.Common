c082ed620594ebf0e579422ae384733a978406b7
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2025-02-22 21:04:06 +0100

    feat(di): FileSystem module (#5)
    Author: Felix Hake

---
+++ tests/Tests/Infrastructure/Extensions/ContainerBuilderExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using SaveApis.Common.Infrastructure.Extensions;
+
+namespace Tests.Infrastructure.Extensions;
+
+[TestFixture]
+public class ContainerBuilderExtensionsTests
+{
+    [Test]
+    public void Can_Register_Module()
+    {
+        // Arrange
+        var builder = new ContainerBuilder();
+
+        // Act & Assert
+        Assert.DoesNotThrow(() => builder.WithModule<TestModule>(args: string.Empty));
+    }
+
+    [Test]
+    public void Cannot_Register_Module_Wrong_Parameter_Count()
+    {
+        // Arrange
+        var builder = new ContainerBuilder();
+
+        // Act & Assert
+        var exception = Assert.Throws<InvalidOperationException>(() => builder.WithModule<TestModule>());
+
+        Assert.That(exception.Message, Is.EqualTo($"Failed to create instance of module {nameof(TestModule)}"));
+    }
+
+    [Test]
+    public void Can_Register_Common_Modules()
+    {
+        // Arrange
+        var builder = new ContainerBuilder();
+
+        // Act & Assert
+        Assert.DoesNotThrow(() => builder.WithCommonModules());
+    }
+}
---
+++ tests/Tests/Infrastructure/Extensions/TestModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+using SaveApis.Common.Infrastructure.DI;
+
+namespace Tests.Infrastructure.Extensions;
+
+public class TestModule(string parameter) : BaseModule
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        Console.WriteLine(parameter);
+    }
+}
