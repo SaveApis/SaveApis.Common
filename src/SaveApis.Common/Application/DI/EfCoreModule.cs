5098a2b3916086ececad7b5b98a1ee636edf43ce
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2025-02-23 22:29:27 +0100

    feat(di): EfCore module (#10)
    Author: Felix Hake

---
+++ src/SaveApis.Common/Application/DI/EfCoreModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore.Design;
+using SaveApis.Common.Infrastructure.DI;
+using SaveApis.Common.Infrastructure.Helper;
+using SaveApis.Common.Infrastructure.Persistence.Sql;
+
+namespace SaveApis.Common.Application.DI;
+
+public class EfCoreModule(IAssemblyHelper helper) : BaseModule
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterAssemblyTypes(helper.GetAssemblies().ToArray())
+            .Where(type => type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IDesignTimeDbContextFactory<>)))
+            .AsImplementedInterfaces()
+            .As<IDesignTimeDbContextFactory<BaseDbContext>>();
+    }
+}
