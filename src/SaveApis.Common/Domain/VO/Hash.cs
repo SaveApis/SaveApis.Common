363b720d50a29490cbee94d01cb3e6caca0733b7
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-02-23 23:37:35 +0100

    feat(di): Add additional libraries
    Author: Felix Hake

---
+++ src/SaveApis.Common/Domain/VO/Hash.cs
@@ -0,0 +1,26 @@
+﻿using Vogen;
+
+namespace SaveApis.Common.Domain.VO;
+
+[ValueObject<string>(conversions: Conversions.NewtonsoftJson | Conversions.EfCoreValueConverter)]
+public partial class Hash
+{
+    public bool Verify(Password password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password.Value, Value);
+    }
+
+    private static string NormalizeInput(string input)
+    {
+        return input;
+    }
+
+    private static Validation Validate(string input)
+    {
+        return string.IsNullOrWhiteSpace(input)
+            ? Validation.Invalid("Hash cannot be empty")
+            : input.Length != 60
+                ? Validation.Invalid("Hash must be 60 characters long")
+                : Validation.Ok;
+    }
+}
---
+++ tests/Tests/ContainerBuilderExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using SaveApis.Common.Application.Helper;
+using SaveApis.Common.Infrastructure.Extensions;
+
+namespace Tests;
+
+[TestFixture]
+public class ContainerBuilderExtensionsTests
+{
+    [Test]
+    public void Can_Register_Common_Modules()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().AddInMemoryCollection().Build();
+        var helper = new AssemblyHelper(Assembly.GetExecutingAssembly());
+        var builder = new ContainerBuilder();
+        
+        // Act
+        Assert.DoesNotThrow(() => builder.WithCommonModules(configuration, helper));
+    }
+}
