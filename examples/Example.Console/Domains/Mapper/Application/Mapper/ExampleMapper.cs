ca86a3a5da3ec1d1dab7acd7fa58e7a7352c3dcd
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-16 16:08:23 +0100

    feat(modules): Mapper Module
    Author: Felix Hake

---
+++ examples/Example.Console/Domains/Mapper/Application/Mapper/ExampleMapper.cs
@@ -0,0 +1,17 @@
+﻿using Example.Console.Domains.Mapper.Domain.Dtos;
+using Example.Console.Domains.Mapper.Domain.Models;
+using Example.Console.Domains.Mapper.Infrastructure.Mapper;
+
+namespace Example.Console.Domains.Mapper.Application.Mapper;
+
+public class ExampleMapper : IExampleMapper
+{
+    public ExampleDto EntityToDto(ExampleModel model)
+    {
+        return new ExampleDto
+        {
+            Id = model.Id,
+            Name = model.Name,
+        };
+    }
+}
