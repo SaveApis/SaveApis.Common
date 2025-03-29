5b51cb5afd92e35112447e1edaaf6504c799fc42
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-29 22:47:23 +0100

    feat: Extension method
    Author: Felix Hake

--- examples/Example.Console/Program.cs
+++ examples/Example.Console/Program.cs
@@ -1,32 +1,15 @@
 ﻿using System.Reflection;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SaveApis.Common.Domains.Core.Application.DI;
 using SaveApis.Common.Domains.Core.Application.Helper;
 using SaveApis.Common.Domains.Core.Infrastructure.Extensions;
-using SaveApis.Common.Domains.EfCore.Application.DI;
-using SaveApis.Common.Domains.FileSystem.Application.DI;
-using SaveApis.Common.Domains.Hangfire.Application.DI;
 using SaveApis.Common.Domains.Hangfire.Infrastructure.Extensions;
-using SaveApis.Common.Domains.Logging.Application.DI;
-using SaveApis.Common.Domains.Mapper.Application.DI;
-using SaveApis.Common.Domains.Mediator.Application.DI;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 var hangfireType = builder.Configuration.GetHangfireType();
 var assemblyHelper = new AssemblyHelper(Assembly.GetExecutingAssembly());
-builder.ConfigureContainer(new AutofacServiceProviderFactory(), containerBuilder =>
-{
-    containerBuilder.WithModule<CoreModule>(null, assemblyHelper);
-    containerBuilder.WithModule<MediatorModule>(null, assemblyHelper);
-    containerBuilder.WithModule<HangfireModule>(null, assemblyHelper, builder.Configuration, hangfireType);
-    containerBuilder.WithModule<EfCoreModule>(null, assemblyHelper);
-    containerBuilder.WithModule<MapperModule>(null, assemblyHelper);
-    containerBuilder.WithModule<FileSystemModule>();
-
-    containerBuilder.WithModule<SerilogModule>(null, builder.Configuration);
-});
+builder.ConfigureContainer(new AutofacServiceProviderFactory(), containerBuilder => containerBuilder.WithCommonModules(assemblyHelper, builder.Configuration, hangfireType));
 
 var app = builder.Build();
 
--- src/SaveApis.Common/Domains/Core/Infrastructure/Extensions/ContainerBuilderExtensions.cs
+++ src/SaveApis.Common/Domains/Core/Infrastructure/Extensions/ContainerBuilderExtensions.cs
@@ -1,5 +1,15 @@
 ﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using SaveApis.Common.Domains.Core.Application.DI;
 using SaveApis.Common.Domains.Core.Infrastructure.DI;
+using SaveApis.Common.Domains.Core.Infrastructure.Helper;
+using SaveApis.Common.Domains.EfCore.Application.DI;
+using SaveApis.Common.Domains.FileSystem.Application.DI;
+using SaveApis.Common.Domains.Hangfire.Application.DI;
+using SaveApis.Common.Domains.Hangfire.Domain.Types;
+using SaveApis.Common.Domains.Logging.Application.DI;
+using SaveApis.Common.Domains.Mapper.Application.DI;
+using SaveApis.Common.Domains.Mediator.Application.DI;
 
 namespace SaveApis.Common.Domains.Core.Infrastructure.Extensions;
 
@@ -20,5 +30,20 @@ public static class ContainerBuilderExtensions
         {
             throw new InvalidOperationException($"Failed to create instance of module {typeof(TModule).Name}", ex);
         }
+    }
+
+    public static ContainerBuilder WithCommonModules(this ContainerBuilder builder, IAssemblyHelper assemblyHelper, IConfiguration configuration, HangfireType hangfireType, Action<ContainerBuilder>? additionalModules = null)
+    {
+        builder.WithModule<CoreModule>(null, assemblyHelper);
+        builder.WithModule<MediatorModule>(null, assemblyHelper);
+        builder.WithModule<HangfireModule>(null, assemblyHelper, configuration, hangfireType);
+        builder.WithModule<EfCoreModule>(null, assemblyHelper);
+        builder.WithModule<MapperModule>(null, assemblyHelper);
+        builder.WithModule<FileSystemModule>();
+        builder.WithModule<SerilogModule>(null, configuration);
+
+        additionalModules?.Invoke(builder);
+
+        return builder;
     }
 }
